e134d73428cadd7b7ac2304162bc82ce8d224182
Jia Yong Zeng <yongabyte@users.noreply.github.com>
2019-01-16 20:49:11 +0000

    Update PieChartController.js

--- PieChartController.js
+++ PieChartController.js
@@ -1,60 +1,9 @@
 ({
     scriptsLoaded : function(component, event, helper) {
-        var labelset=["Loan payment","Property tax","Homeowner's insurance"] ;
-        let loan = component.get("v.monthly") - component.get("v.mortgage.property_tax__c")
-        			- component.get("v.mortgage.homeowners_insurance__c");
-        var dataset=[loan,component.get("v.mortgage.property_tax__c"),
-                     component.get("v.mortgage.homeowners_insurance__c")] ;
-        
-        new Chart(document.getElementById("pie-chart"), {
-            type: 'pie',
-            data: {
-                labels:labelset,
-                datasets: [{
-                    backgroundColor: ["#438bbf", "#ad7ecd","#ff6b90"],
-                    data: dataset
-                }]
-            },
-            options: {
-                legend: {
-                    display: true,
-                    position: 'right'
-                }
-               	,
-                title: {
-                    display: false,
-                    text: 'monthly payment'
-                }                
-            }
-        });
+   		helper.draw(component);
     },
-    draw : function(component, event){
-        var labelset=["Loan payment","Property tax","Homeowner's insurance"] ;
-        let loan = component.get("v.monthly") - component.get("v.mortgage.property_tax__c")
-        			- component.get("v.mortgage.homeowners_insurance__c");
-        var dataset=[loan,component.get("v.mortgage.property_tax__c"),
-                     component.get("v.mortgage.homeowners_insurance__c")] ;
-       
-        new Chart(document.getElementById("pie-chart"), {
-            type: 'pie',
-            data: {
-                labels:labelset,
-                datasets: [{
-                    backgroundColor: ["#438bbf", "#ad7ecd","#ff6b90"],
-                    data: dataset
-                }]
-            },
-            options: {
-                legend: {
-                    display: true,
-                    position: 'right'
-                }
-               	,
-                title: {
-                    display: false,
-                    text: 'monthly payment'
-                }                
-            }
-        });
-  }
+    
+    onMonthlyChanged : function(component, event,helper){
+        helper.draw(component);
+	}
 })
